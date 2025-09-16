55759984f0789b89273286f8a7e940981c6a816f
facterk <facterk6969@yahoo.com>
2025-09-16 20:09:10 +0330

    fixed a minor syntax error

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,7 +5,7 @@ def write_file(working_directory, file_path, content):
         abs_working_dir = os.path.abspath(working_directory)
         abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-        if not abs_file_path.startwith(abs_working_dir):
+        if not abs_file_path.startswith(abs_working_dir):
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
         
         parent_dir = os.path.dirname(abs_file_path)
